ef659e468f7bf460ad34e4d2d989aa5cb7643179
Andrew Parra <andrew.parra@wellspring.com>
2019-01-23 15:18:57 -0600

    Gave TestNG a simple System out TestNG is loaded from Eclipse TestNG Plugin Took out all dependencies

--- src/tests/TC_Practice.java
+++ src/tests/TC_Practice.java
@@ -1,5 +1,11 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class TC_Practice {
 
+    @Test
+    public void Test_Sample() {
+        System.out.println("This is a sample test");
+    }
 }
