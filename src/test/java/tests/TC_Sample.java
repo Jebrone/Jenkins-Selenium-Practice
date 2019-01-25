ba1245dd5a8fb4847072832e4257c59c563b52e3
Andrew Parra <andrew.parra@wellspring.com>
2019-01-25 12:08:27 -0600

    New Restructured Maven Project

---
+++ src/test/java/tests/TC_Sample.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class TC_Sample {
+
+    @Test
+    public void test_SampleOutput() {
+        System.out.println("This is a sample test!");
+    }
+}
