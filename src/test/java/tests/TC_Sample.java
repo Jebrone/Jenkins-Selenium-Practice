0f998038149c4ff3a9eaf36688caf5b7d371a88e
Andrew Parra <andrew.parra@wellspring.com>
2019-01-25 14:38:10 -0600

    Added Selenium for testing

--- src/test/java/tests/TC_Sample.java
+++ src/test/java/tests/TC_Sample.java
@@ -1,11 +1,29 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TC_Sample {
+    WebDriver driver;
 
+    @BeforeMethod
+    public void setup_Driver() {
+         driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void teardown_Driver() {
+        driver.quit();
+    }
+    
     @Test
     public void test_SampleOutput() {
+        driver.navigate().to("https://www.google.com");
         System.out.println("This is a sample test!");
+        System.out.println("This is the Website: " + driver.getTitle());
+        System.out.println("This is the URL: " + driver.getCurrentUrl());
     }
 }
